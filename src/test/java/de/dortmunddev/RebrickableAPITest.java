4cd6bcfeae3931b54cfa61d486afca9658031fa8
Dustin H. <info@dortmunddev.de>
2024-08-28 21:43:08 +0200

    Added test for color endpoint

---
+++ src/test/java/de/dortmunddev/RebrickableAPITest.java
@@ -0,0 +1,60 @@
+package de.dortmunddev;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import de.dortmunddev.rebrickable.dto.color.ColorDTO;
+import de.dortmunddev.rebrickable.dto.color.ColorResponseDTO;
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class RebrickableAPITest
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public RebrickableAPITest(String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( RebrickableAPITest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testFetchAllColors()
+    {
+        assertTrue( true );
+
+        RebrickableAPI api = new RebrickableAPI();
+
+        try {
+            ColorResponseDTO colorResponse = api.getColorService().fetchAllColors();
+
+            assertTrue(colorResponse.getCount() > 0);
+            assertFalse(colorResponse.getResults().isEmpty());
+
+            colorResponse.getResults().forEach(color -> {
+                assertNotNull(color.getName());
+                assertNotNull(color.getRgb());
+                assertNotNull(color.getExternalIds());
+            });
+
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
